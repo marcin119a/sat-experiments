5df7532f2dd1285035014e90252d65ec5d22ef2b
Marcin Wierzbiński <marcin119a@gmail.com>
2020-05-04 21:07:10 +0200

    Update performance.py

--- performance.py
+++ performance.py
@@ -2,18 +2,19 @@ import os
 import tensorflow as tf
 import itertools
 from neuralheuristicsforsat.helpers import chunkIt, save_to_csv
+import time
+from SIMCIM import SIMCim
 
 def predict(params):
-    complexity = 300
-    tfrecord_location = '/content/neuralheuristicsforsat/sr_{0}'.format(complexity)
-    name = "train_21021_sr_{0}.tfrecord".format(complexity)
+    n = params['complexity']
+    tfrecord_location = '/content/sat-experiments/sr_{0}'.format(n)
+    name = "train_{1}_sr_{0}.tfrecord".format(n, params['seed'])
     filename = os.path.join(tfrecord_location, name)
 
     record_iterator = tf.python_io.tf_record_iterator(path=filename)
     preds = []
     # targes = []
-    batch_size = 300
-    n = 300
+    batch_size = params['observations']
 
     train_set = {'cnf': list(), 'sat': list()}
     sim_results = list()
@@ -24,16 +25,13 @@ def predict(params):
 
         m = len(example.features.feature["inputs"].float_list.value) // 2
 
-        inputs = chunkIt(example.features.feature["inputs"].float_list.value,
-                         m)  # split examp: [0,1,2,3,4,5] into [0,1], [2,3], [4,5]
-
+        inputs = chunkIt(example.features.feature["inputs"].float_list.value, m) 
         train_set['cnf'].append(inputs)
         targ = int(example.features.feature["sat"].float_list.value[0])
         train_set['sat'].append(targ)
 
         start = time.time()
-        sim_result = SIMCim(params, n, m,
-                            inputs)  # predict SIMCim, inputs- params ISING Model, n- variables, M-clausures
+        sim_result = SIMCim(params, n, m, inputs)  # predict SIMCim, inputs- params ISING Model, n- variables, M-clausures
 
         end = time.time()
 
