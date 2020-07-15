79fc40d1a635e1da384b8dc7a2521921ca9c1211
Marcin Wierzbiński <marcin119a@gmail.com>
2020-07-15 23:12:29 +0200

    comments

--- maxsat.py
+++ maxsat.py
@@ -21,7 +21,12 @@ def brute_force_2_max_sat(cnf):
             return True, len(cnf)
  
     return cnf_max, m
-    
+
+"""
+Transformation from list format into dictonary format. 
+from [[1,-2], [2,3]] -> {('0000000001', True), ('0000000002', False), ('0000000002', True), ('0000000003', True)}
+
+"""
 def transform(lista):
   result = []
   for cnf in lista:
@@ -33,6 +38,15 @@ def transform(lista):
     result.append(conj)
   return result
 
+"""
+dump into akmaxsat format
+Example:
+p cnf 4 20
+4 -1 0
+-17 6 0
+-19 -1 0
+1 -1 0
+"""
 def dump(input_list, m, n):
   f = open("a{0}.txt".format(m),"a+")    
   f.write("p cnf %d %d\n" % (len(input_list), n))
@@ -42,7 +56,16 @@ def dump(input_list, m, n):
     f.write("%d %d %d\n" % (a, b, 0))
   f.close()
 
-def function_from_akmaxsat(out):
+"""
+From out of akmaxsat format like: 
+14 -4 -20
+into 
+{('0000000014', True), ('0000000004', False), ('0000000020', False)}
+
+"""
+def function_from_akmaxsat(out_string):
+  out_sting = out_sting.split(" ")
+  out = [int(x) for x in a]
   conj = set()
   for x in out:
       conj.add((
